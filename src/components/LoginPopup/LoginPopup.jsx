17d9d18d10317c28c55f18bf274a46346fd2e3a7
shamshad chachu <chachushamshad0987@gmail.com>
2025-07-29 16:40:14 +0530

    css name chnaged

--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import './LoginPopup.css'
+import './LoginStyle.css'
 import { assets } from '../../assets/assets'
 
 const LoginPopup = ({setShowLogin}) => {
